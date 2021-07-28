f5207ef22ec2d9ee5adfa7df99dc2542f6f0ab72
Mary <mariyapiligrim3@gmail.com>
2021-07-28 12:38:41 +0300

    Add all tasks

--- assets/index.js
+++ assets/index.js
@@ -22,49 +22,63 @@ function getMax(arrayOfNumbers) {
   }
   return maxNumber;
 }
+
+console.log(getMin(arr), getMax(arr));
+
 //Дан массив arr. Найдите среднее арифметическое его элементов. Проверьте задачу на массиве с элементами 12, 15, 20, 25, 59, 79.
-const arr = [12,15,20,25,59,79];
-const average = function() {
-  let result = 0;
-  for(let i=0, i<array.length, i++)
-  result += array[i]
+
+const array = [12, 15, 20, 25, 59, 79];
+function averageNum(array) {
+  sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum / array.length;
 }
-return (result/array.length) 
-
-}
-
-
-
-
+console.log(averageNum(array))
 
 
 //=========(Math.random + Math.ceil/Math.floor/Math.round)==========
 //Выведите на экран случайное целое число от 1 до 100 с помощью функции.
+function getRandom() {
+  return Math.ceil(Math.random() * (100));
+}
+
+console.log(getRandom(100))
+
+
 //Заполните массив 10-ю случайными целыми числами.
+const arrRandom = [];
+for (let i = 0; i < 10; i++) {
+  arrRandom[i] = Math.round(Math.random() * 100);
+}
+
+console.log(arrRandom)
+
 
 //concat
-const array1 = [1,2,3];
-const array2 = [4,5,6];
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
 const array3 = array1.concat(array2);
 
 console.log(array3)
 
 //reverse
-const arrReverse = [1,2,3];
+const arrReverse = [1, 2, 3];
 const reserved = arrReverse.reverse();
 
 console.log(arrReverse)
 
 //push
-const arr123 = [1,2,3];
-const push = arr123.push(4,5,6);
+const arr123 = [1, 2, 3];
+const push = arr123.push(4, 5, 6);
 
 console.log(arr123)
 
 //unshift
-const arr456 = [1,2,3];
-const unshift = [4,5,6];
-arr456.unshift(4,5,6)
+const arr456 = [1, 2, 3];
+const unshift = [4, 5, 6];
+arr456.unshift(4, 5, 6)
 
 console.log(arr456)
 
@@ -83,45 +97,45 @@ console.log(popped)
 console.log(arrPop)
 
 //slice
-const arrSlice = [1,2,3,4,5];
-const sliced = arrSlice.slice(0,3)
+const arrSlice = [1, 2, 3, 4, 5];
+const sliced = arrSlice.slice(0, 3)
 
 console.log(sliced)
 
-const sliced2 = arrSlice.slice(3,5)
+const sliced2 = arrSlice.slice(3, 5)
 
 console.log(sliced2)
 
 //splice
-const arrSplice1 = [1,2,3,4,5];
-const spliced145 = arrSplice1.splice(1,2)
+const arrSplice1 = [1, 2, 3, 4, 5];
+const spliced145 = arrSplice1.splice(1, 2)
 
 console.log(arrSplice1)
 
-const arrSplice2 = [1,2,3,4,5];
-const spliced2 = arrSplice2.splice(0,1);
-const spliced3 = arrSplice2.splice(3,1);
+const arrSplice2 = [1, 2, 3, 4, 5];
+const spliced2 = arrSplice2.splice(0, 1);
+const spliced3 = arrSplice2.splice(3, 1);
 
 console.log(arrSplice2)
 
-const arrSplice3 = [1,2,3,4,5];
-const spliced4 = arrSplice3.splice(3,0,'a', 'b', 'c')
+const arrSplice3 = [1, 2, 3, 4, 5];
+const spliced4 = arrSplice3.splice(3, 0, 'a', 'b', 'c')
 
 console.log(arrSplice3)
 
-const arrSplice4 = [1,2,3,4,5];
-const spliced5 = arrSplice4.splice(1,0, 'a', 'b');
-const spliced6 = arrSplice4.splice(6,0, 'c');
-const spliced7 = arrSplice4.splice(8,0, 'e');
+const arrSplice4 = [1, 2, 3, 4, 5];
+const spliced5 = arrSplice4.splice(1, 0, 'a', 'b');
+const spliced6 = arrSplice4.splice(6, 0, 'c');
+const spliced7 = arrSplice4.splice(8, 0, 'e');
 
 console.log(arrSplice4)
 
 //sort
-const arrSort = [3,4,1,2,7];
+const arrSort = [3, 4, 1, 2, 7];
 arrSort.sort();
 
 console.log(arrSort)
 
 //object.keys
-const objKeys = {js:'test', jq: 'hello', css: 'world'};
+const objKeys = { js: 'test', jq: 'hello', css: 'world' };
 console.log(Object.keys(objKeys));
